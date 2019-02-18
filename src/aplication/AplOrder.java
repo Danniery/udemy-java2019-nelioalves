f323bdf8a811da28cfd16abb15876f7786654900
Danniery David Alves <danniery@hotmail.com>
2019-02-18 17:00:13 -0300

    Commit utilizando o plugin no Eclipse

--- src/aplication/AplOrder.java
+++ src/aplication/AplOrder.java
@@ -16,6 +16,7 @@ public class AplOrder {
 
 	public static void main(String[] args) throws ParseException {
 		
+		System.out.println("Commit usando o plugin do Eclipse");
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
